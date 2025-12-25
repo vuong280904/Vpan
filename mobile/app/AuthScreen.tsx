2514cd1e5237dc0b5e004d1a675f11bfe236e43d
BaokPham27 <baoph2.22it@vku.udn.vn>
2025-12-25 20:33:29 +0700

    Update AuthScreen.tsx

--- mobile/app/AuthScreen.tsx
+++ mobile/app/AuthScreen.tsx
@@ -34,7 +34,7 @@ const BG_IMAGE =
     "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjZqZnZkN3hoM21lODBjaWtmYnVnaWJ4dW15bHlldXRoaDd5eXM1YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6ZtkZeusjPpWaAoM/giphy.gif";
 
 const API_URL = Platform.select({
-    web: "http://localhost:5000/api/auth",
+    web: "http://vpan-api.onrender.com/api/auth",
     ios: "http://192.168.2.7:5000/api/auth",
     android: "http://192.168.2.7:5000/api/auth",
 });
