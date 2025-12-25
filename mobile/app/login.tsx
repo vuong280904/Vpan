e217466307480427e6119ef91165d6257441c63b
BaokPham27 <baoph2.22it@vku.udn.vn>
2025-12-25 20:33:10 +0700

    Update login.tsx

--- mobile/app/login.tsx
+++ mobile/app/login.tsx
@@ -20,7 +20,7 @@ import { useAuth } from "../context/AuthContext";
 const api = axios.create({
   baseURL:
     Platform.OS === "web"
-      ? "http://localhost:5000/api/auth"
+      ? "http://vpan-api.onrender.com/api/auth"
       : "http://192.168.2.7:5000/api/auth", // ← ĐÚNG IP MÁY BẠN HIỆN TẠI
   timeout: 100000,
 });
